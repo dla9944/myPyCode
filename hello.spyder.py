c34023f14adde2b47816d592ccceb6fcc88980e8
Chae Won Im <dla9944@naver.com>
2022-11-21 16:09:38 +0900

    Initial commit

---
+++ hello.spyder.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 11:34:00 2022
+
+@author: user
+"""
+
+print('hello Spyder!!') 
---
+++ hello_spyder.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 14:59:03 2022
+
+@author: 임채원
+"""
+
+print("hello spyder!!")
