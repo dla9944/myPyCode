cfaf65898ada4f91d9ff1676946560817c19d662
Chae Won Im <dla9944@naver.com>
2022-11-22 12:49:48 +0900

    2022.11.22 python 교육
    교육내용 요약

--- hello.spyder.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 21 11:34:00 2022
-
-@author: user
-"""
-
-print('hello Spyder!!') 
--- hello_spyder.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 21 14:59:03 2022
-
-@author: 임채원
-"""
-
-print("hello spyder!!")
